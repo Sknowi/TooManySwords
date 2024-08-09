6feb68a1213ebaf5a2af878a41b05a436b783d70
Snowy <jarlyf123@gmail.com>
2024-08-09 17:39:03 +0200

    yipeeee

--- src/main/java/snowy/redstone/tms/item/custom/LaserSwordOff.java
+++ src/main/java/snowy/redstone/tms/item/custom/LaserSwordOff.java
@@ -1,9 +1,13 @@
 package snowy.redstone.tms.item.custom;
 
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
@@ -27,6 +31,14 @@ public class LaserSwordOff extends SwordItem {
 
         ItemStack replace = ModItems.LASER_SWORD_ON.getDefaultStack();
         replace.setDamage(user.getStackInHand(hand).getDamage());
+
+        for (RegistryEntry<Enchantment> e : user.getStackInHand(hand).getEnchantments().getEnchantments()) {
+
+
+            replace.addEnchantment(e, user.getStackInHand(hand).getEnchantments().getLevel(e));
+
+
+        }
 
         user.setStackInHand(hand, replace);
 
--- src/main/java/snowy/redstone/tms/item/custom/LaserSwordOn.java
+++ src/main/java/snowy/redstone/tms/item/custom/LaserSwordOn.java
@@ -1,5 +1,6 @@
 package snowy.redstone.tms.item.custom;
 
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
@@ -7,6 +8,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
@@ -29,6 +31,14 @@ public class LaserSwordOn extends SwordItem {
         ItemStack replace = ModItems.LASER_SWORD_OFF.getDefaultStack();
         replace.setDamage(user.getStackInHand(hand).getDamage());
 
+        for (RegistryEntry<Enchantment> e : user.getStackInHand(hand).getEnchantments().getEnchantments()) {
+
+
+            replace.addEnchantment(e, user.getStackInHand(hand).getEnchantments().getLevel(e));
+
+
+        }
+
         user.setStackInHand(hand, replace);
 
 
@@ -40,7 +50,7 @@ public class LaserSwordOn extends SwordItem {
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
         super.inventoryTick(stack, world, entity, slot, selected);
 
-        if (selected && entity.isLiving()) {
+        if (selected && entity.isPlayer()) {
             stack.setDamage(stack.getDamage() + 1);
 
             if (stack.getDamage() >= stack.getMaxDamage()) {
@@ -48,6 +58,14 @@ public class LaserSwordOn extends SwordItem {
 
                 ItemStack replace = ModItems.LASER_SWORD_OFF.getDefaultStack();
                 replace.setDamage(stack.getDamage());
+
+                for (RegistryEntry<Enchantment> e : ((PlayerEntity) entity).getInventory().getStack(slot).getEnchantments().getEnchantments()) {
+
+
+                    replace.addEnchantment(e, ((PlayerEntity) entity).getInventory().getStack(slot).getEnchantments().getLevel(e));
+
+
+                }
 
                 ((PlayerEntity)entity).getInventory().setStack(slot, replace);
             }
@@ -60,6 +78,14 @@ public class LaserSwordOn extends SwordItem {
 
             ItemStack replace = ModItems.LASER_SWORD_OFF.getDefaultStack();
             replace.setDamage(stack.getDamage());
+
+            for (RegistryEntry<Enchantment> e : ((PlayerEntity) entity).getInventory().getStack(slot).getEnchantments().getEnchantments()) {
+
+
+                replace.addEnchantment(e, ((PlayerEntity) entity).getInventory().getStack(slot).getEnchantments().getLevel(e));
+
+
+            }
 
             ((PlayerEntity)entity).getInventory().setStack(slot, replace);
         }
